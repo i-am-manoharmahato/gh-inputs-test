ae8c7082aab07bed7e6b8f5113baf71263b5d70d
Manohar Kumar Mahato <83580206+i-am-manoharmahato@users.noreply.github.com>
2023-11-19 19:20:18 +1100

    Update app.tf

--- modules/app.tf
+++ modules/app.tf
@@ -1,16 +1,5 @@
-resource "aws_iam_role" "dummy_role" {
-  name = var.dummy_name
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"  # Adjust as needed based on the service that assumes this role
-        }
-      }
-    ]
-  })
+resource "aws_ssm_parameter" "foo" {
+  name = "foo"
+  type = string
+  value = "bar"
 }
