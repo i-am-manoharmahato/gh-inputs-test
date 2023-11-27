5a1db701088ccee4ce03adadca4989248468ffb7
Manohar Kumar <manohar.mahato@outlook.com>
2023-11-27 16:07:37 +1100

    testing

--- modules/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "github-oidc-terraform-mandy"
-    key            = "${var.dummy_name}/terraform.tfstate"
-    region         = "ap-southeast-2"
-    encrypt        = true
-  }
-}
